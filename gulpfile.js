d6b55ebf4f19735aeead9caba8c512bf19c6eacb
HiimChipshot <jaysonbenavides@gmail.com>
2018-10-12 22:42:14 +0800

    setting up sass structure to fix components html layout

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,33 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
-var uglifycss = require('gulp-uglifycss');
+//var uglifycss = require('gulp-uglifycss');
+var browserSync = require('browser-sync').create();
 
 gulp.task('sass', function () {
   return gulp
+    //.src(["./assets/styles/*.scss", "./assets/styles/**/*.scss"])
     .src("./assets/styles/*.scss")
     .pipe(sass().on("error", sass.logError))
-    .pipe(uglifycss({ uglyComments: true }))
-    .pipe(gulp.dest("./public/css"));
+    //.pipe(uglifycss({ uglyComments: true }))
+    .pipe(gulp.dest("./public/css"))
+    .pipe(browserSync.stream());
 });
+
+// Static Server
+gulp.task('browser-sync', function () {
+  browserSync.init({
+    server: {
+      baseDir: "./"
+    }
+  });
+});
+
 //
 gulp.task('run', ['sass']);
 
 gulp.task('watch', function(){
-  gulp.watch('./assets/styles/*.scss', ['sass']);
+  gulp.watch(["./assets/styles/*.scss", "./assets/styles/**/*.scss"], ['sass']);
+  gulp.watch("./*.html").on('change', browserSync.reload);
 });
 
-gulp.task('default', ['run', 'watch']);
+gulp.task('default', ['run', 'watch', 'browser-sync']);
